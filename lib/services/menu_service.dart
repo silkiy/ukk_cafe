bb4b274755b80494a1126265e85e0640b9354426
silkiy <wildanmilk@gmail.com>
2024-10-23 07:19:12 +0700

    feat (backend) : add function update and delete

--- lib/services/menu_service.dart
+++ lib/services/menu_service.dart
@@ -78,4 +78,77 @@ class MenuService {
       throw Exception('Error fetching player count.');
     }
   }
+
+  Future<void> deleteMenuById(String id) async {
+    try {
+      // Cek apakah menu dengan ID yang diberikan ada
+      DocumentSnapshot docSnapshot =
+          await _firestore.collection('menu_cafe').doc(id).get();
+
+      if (!docSnapshot.exists) {
+        print('Menu with ID: $id does not exist.');
+        throw Exception('Menu not found.');
+      }
+
+      // Lakukan penghapusan dokumen jika menu ada
+      await _firestore.collection('menu_cafe').doc(id).delete();
+      print('Menu with ID: $id successfully deleted');
+    } catch (e) {
+      print('Error deleting menu: $e');
+
+      // Memunculkan pesan kesalahan yang lebih spesifik
+      if (e is FirebaseException && e.code == 'permission-denied') {
+        throw Exception(
+            'Permission denied. You do not have permission to delete this menu.');
+      } else {
+        throw Exception('Failed to delete menu. Error: $e');
+      }
+    }
+  }
+
+  Future<void> updateMenuById({
+    required String id,
+    String? imageUrl,
+    String? nama,
+    String? jenis,
+    String? hargaMenu,
+    String? deskripsi,
+  }) async {
+    try {
+      // Cek apakah menu dengan ID yang diberikan ada
+      DocumentSnapshot docSnapshot =
+          await _firestore.collection('menu_cafe').doc(id).get();
+
+      if (!docSnapshot.exists) {
+        print('Menu with ID: $id does not exist.');
+        throw Exception('Menu not found.');
+      }
+
+      // Membuat map untuk data yang akan diperbarui
+      Map<String, dynamic> updatedData = {};
+
+      if (imageUrl != null) updatedData['img'] = imageUrl;
+      if (nama != null) updatedData['name'] = nama;
+      if (jenis != null) updatedData['jenis'] = jenis;
+      if (hargaMenu != null) {
+        num harga = num.parse(hargaMenu); // Parsing hargaMenu ke num
+        updatedData['harga'] = harga;
+      }
+      if (deskripsi != null) updatedData['deskripsi'] = deskripsi;
+
+      // Melakukan pembaruan dokumen
+      await _firestore.collection('menu_cafe').doc(id).update(updatedData);
+      print('Menu with ID: $id successfully updated');
+    } catch (e) {
+      print('Error updating menu: $e');
+
+      // Memunculkan pesan kesalahan yang lebih spesifik
+      if (e is FirebaseException && e.code == 'permission-denied') {
+        throw Exception(
+            'Permission denied. You do not have permission to update this menu.');
+      } else {
+        throw Exception('Failed to update menu. Error: $e');
+      }
+    }
+  }
 }
