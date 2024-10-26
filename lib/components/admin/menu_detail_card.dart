23f5f816c4bc0e6bf65fbbc061bf6722dc9a9eb6
silkiy <wildanmilk@gmail.com>
2024-10-27 01:36:30 +0700

    fix (frontend) : change CircleAvatar to ClipOval, change Column to Expanded and add boxShadow

--- lib/components/admin/menu_detail_card.dart
+++ lib/components/admin/menu_detail_card.dart
@@ -19,40 +19,56 @@ class MenuDetailCard extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
       ),
       child: Padding(
         padding: EdgeInsets.all(20),
         child: Row(
           children: [
-            CircleAvatar(
-              backgroundColor: Colors.black,
-              backgroundImage: NetworkImage(
+            ClipOval(
+              child: Image.network(
                 imagePath,
+                width: 60, // Set a fixed width and height
+                height: 60,
+                fit: BoxFit.cover, // Cover the circle
+                errorBuilder: (context, error, stackTrace) {
+                  return Icon(
+                    Icons.fastfood,
+                    size: 60,
+                  ); // Placeholder icon
+                },
               ),
-              radius: 30,
             ),
             SizedBox(width: 20),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  namaMenu,
-                  style: GoogleFonts.poppins(
-                    fontSize: MediaQuery.of(context).size.width * 0.035,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    namaMenu,
+                    style: GoogleFonts.poppins(
+                      fontSize: MediaQuery.of(context).size.width * 0.035,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-                Text(
-                  jenisMenu,
-                  style: GoogleFonts.poppins(
-                    fontSize: MediaQuery.of(context).size.width * 0.035,
-                    fontWeight: FontWeight.w400,
-                    color: const Color.fromRGBO(60, 60, 60, 1),
+                  Text(
+                    jenisMenu,
+                    style: GoogleFonts.poppins(
+                      fontSize: MediaQuery.of(context).size.width * 0.03,
+                      fontWeight: FontWeight.w400,
+                      color: const Color.fromRGBO(60, 60, 60, 1),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
