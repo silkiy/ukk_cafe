cb2f52bbb868a84f1b91be26fa4b827ab1e01333
silkiy <wildanmilk@gmail.com>
2024-10-27 01:46:25 +0700

    chore : change typo text

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -36,13 +36,13 @@ class _LoginPageState extends State<LoginPage> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('kelasahan'),
+          title: Text('Kesalahan'),
           content: Text(message),
           actions: [
             TextButton(
               child: Text('OK'),
               onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
+                Navigator.of(context).pop();
               },
             ),
           ],
@@ -55,7 +55,6 @@ class _LoginPageState extends State<LoginPage> {
     final email = _emailController.text;
     final password = _passwordController.text;
 
-    // Call the signIn method from AuthService
     AuthService().signIn(
       email,
       password,
@@ -69,176 +68,201 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(243, 244, 248, 1),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.15,
-              ),
-              Icon(
-                Icons.coffee,
-                size: 100,
-                color: Color.fromARGB(255, 53, 35, 23),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Text(
-                "Wikusama Cafe",
-                style: GoogleFonts.bebasNeue(
-                  fontSize: MediaQuery.of(context).size.width * 0.125,
-                ),
-              ),
-              SizedBox(
-                height: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 25,
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.shade400,
-                        offset: Offset(0.4, 0.4),
-                        blurRadius: 0.2,
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color.fromRGBO(210, 210, 210, 1),
-                        ),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blueAccent),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      hintText: "Masukan Email Anda",
-                      hintStyle: GoogleFonts.poppins(
-                        fontSize: MediaQuery.of(context).size.width * 0.03,
-                        fontWeight: FontWeight.w400,
-                        color: Color.fromRGBO(154, 154, 154, 1),
-                      ),
-                      fillColor: Colors.white,
-                      filled: true,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 15),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 25,
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey,
-                        offset: Offset(1, 1),
-                        blurRadius: 2,
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color.fromRGBO(210, 210, 210, 1),
-                        ),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blueAccent),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      hintText: "Masukan Password Anda",
-                      hintStyle: GoogleFonts.poppins(
-                        fontSize: MediaQuery.of(context).size.width * 0.03,
-                        fontWeight: FontWeight.w400,
-                        color: Color.fromRGBO(154, 154, 154, 1),
-                      ),
-                      fillColor: Colors.white,
-                      filled: true,
-                      suffixIcon: const Icon(
-                        Icons.lock,
-                        color: Color.fromRGBO(154, 154, 154, 1),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 35),
-
-              //Log In Button
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25),
-                child: GestureDetector(
-                  onTap: _isLoading ? null : _handleSignIn,
-                  child: Container(
-                    height: MediaQuery.of(context).size.width * 0.14,
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(203, 24, 28, 1),
-                      borderRadius: BorderRadius.circular(12),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey,
-                          offset: Offset(1, 1),
-                          blurRadius: 2,
-                        ),
-                      ],
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Login",
-                        style: GoogleFonts.poppins(
-                          fontSize: MediaQuery.of(context).size.width * 0.04,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        children: [
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  GestureDetector(
-                    onTap: () async {
-                      Navigator.pushReplacementNamed(context, '/landing_page');
-                    },
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.15,
+                  ),
+                  Icon(
+                    Icons.coffee,
+                    size: 100,
+                    color: Color.fromARGB(255, 53, 35, 23),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    "Wikusama Cafe",
+                    style: GoogleFonts.bebasNeue(
+                      fontSize: MediaQuery.of(context).size.width * 0.125,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
                     child: Container(
-                      child: Text(
-                        "Back to landing page",
-                        style: GoogleFonts.poppins(
-                          fontSize: MediaQuery.of(context).size.width * 0.035,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromRGBO(203, 24, 28, 1),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.shade400,
+                            offset: Offset(0.4, 0.4),
+                            blurRadius: 0.2,
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide:
+                                BorderSide.none, // Menghilangkan border default
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(
+                                  203, 24, 28, 1), // Warna border saat fokus
+                              width: 2.0,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(
+                              color:
+                                  Colors.grey, // Warna border saat tidak fokus
+                              width: 1.0,
+                            ),
+                          ),
+                          filled: true,
+                          fillColor:
+                              Colors.white, // Warna background TextFormField
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.shade400,
+                            offset: Offset(0.4, 0.4),
+                            blurRadius: 0.2,
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide:
+                                BorderSide.none, // Menghilangkan border default
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(
+                                  203, 24, 28, 1), // Warna border saat fokus
+                              width: 2.0,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: BorderSide(
+                              color:
+                                  Colors.grey, // Warna border saat tidak fokus
+                              width: 1.0,
+                            ),
+                          ),
+                          filled: true,
+                          fillColor:
+                              Colors.white, // Warna background TextFormField
                         ),
                       ),
                     ),
+                  ),
+                  SizedBox(height: 35),
+
+                  // Log In Button
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25),
+                    child: GestureDetector(
+                      onTap: _isLoading ? null : _handleSignIn,
+                      child: Container(
+                        height: MediaQuery.of(context).size.width * 0.14,
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(203, 24, 28, 1),
+                          borderRadius: BorderRadius.circular(16),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              offset: Offset(1, 1),
+                              blurRadius: 2,
+                            ),
+                          ],
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Login",
+                            style: GoogleFonts.poppins(
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.04,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () async {
+                          Navigator.pushReplacementNamed(
+                            context,
+                            '/landing_page',
+                          );
+                        },
+                        child: Container(
+                          child: Text(
+                            "Back to landing page",
+                            style: GoogleFonts.poppins(
+                              fontSize:
+                                  MediaQuery.of(context).size.width * 0.035,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromRGBO(203, 24, 28, 1),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
+
+          // Full-Screen Loading Indicator
+          if (_isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
