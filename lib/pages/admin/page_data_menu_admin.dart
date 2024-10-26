2391bd24710d933df12c20d9fc88e2a987ef8aac
silkiy <wildanmilk@gmail.com>
2024-10-27 01:42:50 +0700

    fix (frontend) :  change ui in PageDataMenuAdmin

--- lib/pages/admin/page_data_menu_admin.dart
+++ lib/pages/admin/page_data_menu_admin.dart
@@ -18,7 +18,7 @@ class _PageDataMenuAdminState extends State<PageDataMenuAdmin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(243, 244, 248, 1),
+      backgroundColor: const Color.fromRGBO(243, 244, 248, 1),
       appBar: AppBar(
         backgroundColor: Colors.grey[100],
         title: Text(
@@ -30,133 +30,65 @@ class _PageDataMenuAdminState extends State<PageDataMenuAdmin> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Color.fromRGBO(203, 24, 28, 1),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10),
-                  ),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(15),
-                  child: Text(
-                    "Data Menu",
-                    style: GoogleFonts.poppins(
-                      fontSize: MediaQuery.of(context).size.width * 0.035,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                    ),
-                  ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(203, 24, 28, 1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              padding: const EdgeInsets.all(15),
+              child: Text(
+                "Data Menu",
+                style: GoogleFonts.poppins(
+                  fontSize: MediaQuery.of(context).size.width * 0.035,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
                 ),
               ),
-              Container(
-                width: double.infinity,
-                color: Color.fromRGBO(152, 3, 6, 1),
-                child: Padding(
-                  padding: EdgeInsets.all(15),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Expanded(
-                        flex: 3,
-                        child: Text(
-                          "Nama Menu",
-                          style: GoogleFonts.poppins(
-                            fontSize: MediaQuery.of(context).size.width * 0.03,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 4,
-                        child: Text(
-                          "Jenis",
-                          style: GoogleFonts.poppins(
-                            fontSize: MediaQuery.of(context).size.width * 0.03,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Text(
-                          "Details",
-                          style: GoogleFonts.poppins(
-                            fontSize: MediaQuery.of(context).size.width * 0.03,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              FutureBuilder<List<Menu>>(
-                future: _menuService.getMenu(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (snapshot.hasError) {
-                    return Center(
-                      child: Text('Error: ${snapshot.error}'),
-                    );
-                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                    return Center(
-                      child: Text('Tidak ada menu yang ditemukan'),
-                    );
-                  }
-              
-                  final menuList = snapshot.data!;
-              
-                  return Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10),
-                      ),
-                    ),
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                      physics:
-                          NeverScrollableScrollPhysics(), // Prevent scrolling
-                      itemCount: menuList.length,
-                      itemBuilder: (context, index) {
-                        final menu = menuList[index];
-                        return Column(
-                          children: [
-                            InformasiMenuAdmin(
-                              namaMenu: menu.name,
-                              jenis: menu.jenis,
-                              id: menu.id,
-                              img: menu.img,
-                              harga: menu.harga.toString(),
-                              deskripsi: menu.deskripsi,
-                            ),
-                            if (index != menuList.length - 1)
-                              SizedBox(height: 5), // Add space between items
-                          ],
-                        );
-                      },
-                    ),
+            ),
+            const SizedBox(height: 20),
+            FutureBuilder<List<Menu>>(
+              future: _menuService.getMenu(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
                   );
-                },
-              ),
-            ],
-          ),
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(
+                    child: Text('Tidak ada menu yang ditemukan'),
+                  );
+                }
+
+                final menuList = snapshot.data!;
+
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: menuList.length,
+                    itemBuilder: (context, index) {
+                      final menu = menuList[index];
+                      return InformasiMenuAdmin(
+                        namaMenu: menu.name,
+                        jenis: menu.jenis,
+                        id: menu.id,
+                        img: menu.img,
+                        harga: menu.harga.toString(),
+                        deskripsi: menu.deskripsi,
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
