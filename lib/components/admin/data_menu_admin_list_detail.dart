b4f66408c72e723d840715ab427d91cd2ec151c0
silkiy <wildanmilk@gmail.com>
2024-10-27 01:31:29 +0700

    fix (frontend) : update padding, font size

--- lib/components/admin/data_menu_admin_list_detail.dart
+++ lib/components/admin/data_menu_admin_list_detail.dart
@@ -12,30 +12,33 @@ class DataMenuAdminListDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(
-          "$type : ",
-          style: GoogleFonts.poppins(
-            fontSize: MediaQuery.of(context).size.width * 0.035,
-            fontWeight: FontWeight.w600,
-            color: Color.fromRGBO(37, 37, 37, 1),
-          ),
-        ),
-        SizedBox(width: MediaQuery.of(context).size.width * 0.01),
-        Flexible(
-          child: Text(
-            detail,
-            maxLines: 4,
-            overflow: TextOverflow.ellipsis,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        children: [
+          Text(
+            "$type:",
             style: GoogleFonts.poppins(
               fontSize: MediaQuery.of(context).size.width * 0.035,
-              fontWeight: FontWeight.w400,
-              color: Color.fromRGBO(60, 60, 60, 1),
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(37, 37, 37, 1),
             ),
           ),
-        ),
-      ],
+          SizedBox(width: 8),
+          Expanded(
+            child: Text(
+              detail,
+              maxLines: 4,
+              overflow: TextOverflow.ellipsis,
+              style: GoogleFonts.poppins(
+                fontSize: MediaQuery.of(context).size.width * 0.03,
+                fontWeight: FontWeight.w400,
+                color: Color.fromRGBO(60, 60, 60, 1),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
